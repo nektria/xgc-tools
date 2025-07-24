087f15c44c3237634e5a5f2233004b803460f2ac
Javier Gonzalez <xgc1986@gmail.com>
2025-07-24 10:58:00 +0200

    autocommit

---
+++ src/Exception/ResourceNotFoundException.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xgc\Exception;
+
+class ResourceNotFoundException extends BaseException
+{
+    public function __construct(string $resourceType, ?string $ref)
+    {
+        parent::__construct(
+            message: $ref === null
+                ? "{$resourceType} not found."
+                : "{$resourceType} '{$ref}' not found.",
+            status: 404,
+            extras: [
+                'resource' => $resourceType,
+                'ref' => $ref,
+            ]
+        );
+    }
+}
