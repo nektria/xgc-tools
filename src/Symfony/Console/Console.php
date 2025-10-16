63186d390979ba21764224c5e03dfdbb9ff54a39
Javi <javi@linux.xgc>
2025-10-16 17:42:35 +0200

    autocommit

--- src/Symfony/Console/Console.php
+++ src/Symfony/Console/Console.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace Xgc\Symfony\Console;
 
 use Symfony\Component\Console\Command\Command as BaseCommand;
-use Symfony\Component\Console\Cursor;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Messenger\Stamp\DelayStamp;
 use Throwable;
 use Xgc\Dto\Document;
@@ -20,13 +17,8 @@ use Xgc\Message\BusInterface;
 use Xgc\Message\Command;
 use Xgc\Message\Query;
 use Xgc\Message\RetryStamp;
+use Xgc\Symfony\Service\OutputService;
 use Xgc\Utils\ContainerBoxTrait;
-use Xgc\Utils\StringUtil;
-
-use function count;
-use function in_array;
-
-use const PHP_EOL;
 
 abstract class Console extends BaseCommand
 {
@@ -34,74 +26,13 @@ abstract class Console extends BaseCommand
 
     private ?InputInterface $input;
 
-    private ?OutputInterface $output;
+    private OutputService $output;
 
     public function __construct(string $name)
     {
         parent::__construct($name);
         $this->input = null;
-        $this->output = null;
-    }
-
-    /**
-     * @param string[]|null $validResponses
-     * @param string[] $autocomplete
-     * @param callable(string): bool|null $cb
-     */
-    protected function ask(
-        string $question,
-        ?array $validResponses = null,
-        ?string $default = null,
-        array $autocomplete = [],
-        ?callable $cb = null
-    ): string {
-        $pre = '';
-        if (count($validResponses ?? []) > 0) {
-            $group = implode(',', $validResponses);
-            $pre = " [{$group}]";
-            if ($default !== '') {
-                $pre .= "({$default}) ";
-            }
-        } elseif ($default !== '' && $default !== null) {
-            $pre .= "({$default}) ";
-        } else {
-            $pre = ' ';
-        }
-
-        $repeatQuestion = true;
-        do {
-            if ($repeatQuestion && $question !== '') {
-                $this->output()->write($question . PHP_EOL . ' <white2>></white2>' . $pre);
-            } else {
-                $this->output()->write(' <white2>></white2>' . $pre);
-            }
-
-            /** @var QuestionHelper $helper */
-            $helper = $this->getHelper('question');
-            $realQuestion = new Question('', $default);
-            $realQuestion->setAutocompleterValues($autocomplete);
-            $realQuestion->setTrimmable(true);
-            $response = StringUtil::trim(
-                $helper->ask($this->input(), $this->output(), $realQuestion) ?? $default ?? '',
-            );
-
-            $valid = true;
-            $validCb = true;
-
-            if ($cb !== null) {
-                $validCb = $cb($response);
-            }
-            if ($validResponses !== null) {
-                $valid = in_array($response, $validResponses, true);
-            }
-
-            if ($default === null && $response === '') {
-                $valid = false;
-            }
-            $repeatQuestion = $response === '?';
-        } while (!($valid && $validCb));
-
-        return StringUtil::trim($response);
+        $this->output = new OutputService();
     }
 
     protected function beep(): void
@@ -109,20 +40,19 @@ abstract class Console extends BaseCommand
         $this->output()->write("\007");
     }
 
-    protected function bus(): BusInterface
+    protected function output(): OutputService
     {
-        return $this->get(BusInterface::class);
+        return $this->output;
     }
 
     protected function clear(): void
     {
-        $this->output()->write("\033\143");
+        $this->output()->clearLine();
     }
 
     protected function clearPreviousLine(): void
     {
-        $this->cursor()->moveUp();
-        $this->cursor()->clearLine();
+        $this->output()->clearPreviousLine();
     }
 
     protected function copy(string $text): bool
@@ -133,11 +63,6 @@ abstract class Console extends BaseCommand
         return $status === 0;
     }
 
-    protected function cursor(): Cursor
-    {
-        return new Cursor($this->output());
-    }
-
     protected function dispatchCommand(
         Command $command,
         bool $async = false,
@@ -145,6 +70,11 @@ abstract class Console extends BaseCommand
         ?RetryStamp $retryOptions = null
     ): void {
         $this->bus()->dispatchCommand($command, $async ? 'system' : null, $delayMs, $retryOptions);
+    }
+
+    protected function bus(): BusInterface
+    {
+        return $this->get(BusInterface::class);
     }
 
     /**
@@ -164,17 +94,17 @@ abstract class Console extends BaseCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->input = $input;
-        $this->output = $output;
+        $this->output->assignOutput($output);
 
         $colors = ['red', 'blue', 'green', 'black', 'yellow', 'magenta', 'cyan', 'white'];
 
         foreach ($colors as $color) {
-            $output->getFormatter()->setStyle("{$color}", new OutputFormatterStyle("{$color}", null, []));
-            $output->getFormatter()->setStyle("{$color}1", new OutputFormatterStyle("{$color}", null, ['bold']));
-            $output->getFormatter()->setStyle("{$color}2", new OutputFormatterStyle("{$color}", null, ['underscore']));
-            $output->getFormatter()->setStyle("{$color}3", new OutputFormatterStyle("{$color}", null, ['blink']));
-            $output->getFormatter()->setStyle("{$color}4", new OutputFormatterStyle("{$color}", null, ['reverse']));
-            $output->getFormatter()->setStyle("{$color}5", new OutputFormatterStyle("{$color}", null, ['conceal']));
+            $output->getFormatter()->setStyle($color, new OutputFormatterStyle($color, null, []));
+            $output->getFormatter()->setStyle("{$color}1", new OutputFormatterStyle($color, null, ['bold']));
+            $output->getFormatter()->setStyle("{$color}2", new OutputFormatterStyle($color, null, ['underscore']));
+            $output->getFormatter()->setStyle("{$color}3", new OutputFormatterStyle($color, null, ['blink']));
+            $output->getFormatter()->setStyle("{$color}4", new OutputFormatterStyle($color, null, ['reverse']));
+            $output->getFormatter()->setStyle("{$color}5", new OutputFormatterStyle($color, null, ['conceal']));
         }
 
         $this->play();
@@ -182,14 +112,11 @@ abstract class Console extends BaseCommand
         return 0;
     }
 
-    protected function forceEnterToContinue(): void
+    abstract protected function play(): void;
+
+    protected function readArgument(string $name): string
     {
-        $this->output()->write('<white2>[ENTER to continue]</white2>');
-        /** @var QuestionHelper $helper */
-        $helper = $this->getHelper('question');
-        $realQuestion = new Question('');
-        $realQuestion->setTrimmable(true);
-        $helper->ask($this->input(), $this->output(), $realQuestion);
+        return $this->input()->getArgument($name);
     }
 
     protected function input(): InputInterface
@@ -199,21 +126,5 @@ abstract class Console extends BaseCommand
         }
 
         return $this->input;
-    }
-
-    protected function output(): OutputInterface
-    {
-        if ($this->output === null) {
-            throw new BaseException('play method has not been executed.');
-        }
-
-        return $this->output;
-    }
-
-    abstract protected function play(): void;
-
-    protected function readArgument(string $name): string
-    {
-        return $this->input()->getArgument($name);
     }
 }
