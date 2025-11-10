d6858e4815d0d1091e1dbd57866831468f7290e3
Javi <javi@linux.xgc>
2025-11-10 17:17:40 +0100

    autocommit

---
+++ src/Exception/PayumentRequiredException.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xgc\Exception;
+
+class PayumentRequiredException extends BaseException
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'Payment is required for this feature',
+            status: 402,
+            extras: [
+                'type' => 'PAYMENT_REQUIRED'
+            ]
+        );
+    }
+}
